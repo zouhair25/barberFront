8476a9548f01b1f55938e1a12830988ce538cbb1
FASSIH Zouhair <z.fassih@akdital.ma>
2026-06-02 15:18:15 +0100

    [add] client

--- src/app/core/models/client.model.ts
+++ src/app/core/models/client.model.ts
@@ -1,15 +1,22 @@
-import { CentreSoin } from "./centre-soin.model";
 import { Page } from "./page.model";
-import { User } from "./user.model";
+
+export interface ClientUser {
+  id: number;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  phone?: string;
+}
 
 export interface Client {
   id: number;
-  user?: User;
-  centreSoin?: CentreSoin;
+  user?: ClientUser;
 }
-
 
 export interface ClientResponse {
   content: Client[];
-  page: Page;
+  totalElements: number;
+  totalPages: number;
+  size: number;
+  number: number;
 }
